bf22f7dec08b62726c5f2f4d44bfd75053eb10cb
bm16ton <bm16ton@gmail.com>
2020-12-20 17:12:17 -0500

    cleanups

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -224,23 +224,23 @@ static const struct ieee80211_regdomain world_regdom = {
 	.alpha2 =  "00",
 	.reg_rules = {
 		/* IEEE 802.11b/g, channels 1..11 */
-		REG_RULE(2312-10, 2462+10, 40, 6, 30, 0),
+		REG_RULE(2312-10, 2462+10, 80, 6, 33, 0),
 		/* IEEE 802.11b/g, channels 12..13. */
-		REG_RULE(2467-10, 2472+10, 20, 6, 30, 0),
+		REG_RULE(2467-10, 2472+10, 80, 6, 33, 0),
 		/* IEEE 802.11 channel 14 - Only JP enables
 		 * this and for 802.11b only */
-		REG_RULE(2484-10, 2552+10, 20, 6, 30, 0),
+		REG_RULE(2484-10, 2552+10, 80, 6, 33, 0),
 		/* IEEE 802.11a, channel 36..48 */
-		REG_RULE(5180-10, 5260+10, 80, 6, 30, 0),
+		REG_RULE(4000-10, 5260+10, 80, 6, 33, 0),
 
 		/* IEEE 802.11a, channel 52..64 - DFS required */
-		REG_RULE(5260-10, 5320+10, 160, 6, 30, 0),
+		REG_RULE(5260-10, 5320+10, 160, 6, 33, 0),
 
 		/* IEEE 802.11a, channel 100..144 - DFS required */
-		REG_RULE(5320-10, 5720+10, 160, 6, 30, 0),
+		REG_RULE(5320-10, 5720+10, 160, 6, 33, 0),
 
 		/* IEEE 802.11a, channel 149..165 */
-		REG_RULE(5720-10, 6000+10, 160, 6, 30, 0),
+		REG_RULE(5720-10, 6000+10, 160, 6, 33, 0),
 
 		/* IEEE 802.11ad (60GHz), channels 1..3 */
 		REG_RULE(56160+2160*1-1080, 56160+2160*3+1080, 2160, 0, 0, 0),
@@ -2860,7 +2860,6 @@ bool reg_dfs_domain_same(struct wiphy *wiphy1, struct wiphy *wiphy2)
 static void reg_copy_dfs_chan_state(struct ieee80211_channel *dst_chan,
 				    struct ieee80211_channel *src_chan)
 {
-
 		dst_chan->dfs_state = src_chan->dfs_state;
 		dst_chan->dfs_state_entered = src_chan->dfs_state_entered;
 
